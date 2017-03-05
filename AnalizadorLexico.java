5da19947b58f6b25ff861fa8d670e96111a30544
carlosgasa <carlos.oficial.uaz@gmail.com>
2017-03-05 13:11:19 -0600

    Con una nueva forma de imprimir datos

--- AnalizadorLexico.java
+++ AnalizadorLexico.java
@@ -1,7 +1,17 @@
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class AnalizadorLexico{
+
 	
-	public static void Main(String a[]){
+	public static void main(String[] a){
+		PrintWriter so = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		so.print("hello");
+		//so.print("Imprime sin salto de linea");
+		//so.println("Imprime con salto de linea");
 		
+		//Al finalizar
+		so.close();
 	}
 }
