9398d59ca0dc2c1416b10b732cddde4b0d69353b
carlosgasa <carlos.oficial.uaz@gmail.com>
2017-03-05 14:56:34 -0600

    Ya abre un archivo .p10

--- AnalizadorLexico.java
+++ AnalizadorLexico.java
@@ -1,17 +1,24 @@
-import java.io.PrintWriter;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
+import java.io.BufferedReader ;
+import java.io.FileReader;
 
 public class AnalizadorLexico{
 
-	
 	public static void main(String[] a){
-		PrintWriter so = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-		so.print("hello");
-		//so.print("Imprime sin salto de linea");
-		//so.println("Imprime con salto de linea");
 		
-		//Al finalizar
-		so.close();
+		leerCodigo("prog1.p10");
+	}
+	
+	public static void leerCodigo(String nombre){
+		try{
+			FileReader codigo = new FileReader(nombre);
+			BufferedReader buffer = new BufferedReader(codigo);
+			String codigoString;
+			
+			while((codigoString = buffer.readLine()) != null){
+				System.out.println(codigoString + "");
+			}
+		}catch(Exception ex){
+			System.out.println("Error al abrir el archivo");
+		}
 	}
 }
