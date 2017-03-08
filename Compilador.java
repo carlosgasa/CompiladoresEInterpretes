b1a476bbf57fcdc9105d256fc8a37c20fb74586e
carlosgasa <carlos.oficial.uaz@gmail.com>
2017-03-08 16:28:25 -0600

    Organizar  y limpiar el codigo.
    Clase principal.

--- Compilador.java
+++ Compilador.java
@@ -1,36 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.io.IOException;
 
 public class Compilador{
 
 	public static void main(String[] args){
 
-		 try {
-			 BufferedReader bf = leerCodigo("prog1.p10");
-			 AnalizadorLexico a = new AnalizadorLexico(bf);
-			 Yytoken token = null;
-			 if(bf != null) System.out.println("#TOKEN\t\tLEXEMA");
+		 try {//Intentar abrir el archivo .p10
+			 FileReader codigo = new FileReader("prog1.p10");//Leer archivo a analizar
+			 BufferedReader buffer = new BufferedReader(codigo);//Pasar al buffer los datos leidos en "codigo"
+			 AnalizadorLexico analizador = new AnalizadorLexico(buffer); //crear el analizador para poder acceder a los tokens
+			 Yytoken token = null;//Tipo de dato para poder acceder a cada token de los guardados en 'analizador'
+			 if(buffer != null) System.out.println("#TOKEN\t\tLEXEMA");//Imprimir el nombre de las columnas, si se logro abrir el archivo,sino pasa hasta el catch
 			 do {
-				 token = a.nextToken();
-				 System.out.println(token.tipo +"\t\t" +token.token);
-			} while (token != null);
+				 token = analizador.nextToken();//Avanzar al siguiente token, extrayendolo de a
+				 System.out.println(token.tipo +"\t\t" +token.token); //Token tiene las propiedades  (int numToken,String token, String tipo, int linea, int columna)
+			} while (token != null);															//																contador de tokens, token,    tipo de token, linea,    columna
 		}
 		catch (Exception ex) {
 			System.out.println("Error al abrir el archivo de codigo");
 		} 
-	}
-	
-	public static BufferedReader leerCodigo(String nombre){
-		
-		try{
-			FileReader codigo = new FileReader(nombre);
-			BufferedReader buffer = new BufferedReader(codigo);
-			return buffer;
-		}catch(Exception ex){
-			return null;
-		}
 	}
 }
